9e7a678f6f7dee4460741e35a72e8c00c67012d2
vaibhavsharma <vaibhav.sharma.jp@gmail.com>
2019-09-14 22:30:56 +0530

    initial commit

--- TextFieldKeybordHandler/ViewController.swift
+++ TextFieldKeybordHandler/ViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var activeTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        tapGesture.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(tapGesture)
     }
-
-
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        self.activeTextField.resignFirstResponder()
+    }
+    
+    @objc func keyboardWillShow(notification:NSNotification) {
+        manageContentInset(notification: notification)
+    }
+    
+    @objc func keyboardWillHide(notification:NSNotification) {
+        resetContentInset()
+    }
+    
+    func manageContentInset(notification:NSNotification) {
+        UIView.animate(withDuration: 0.2) {
+            let userInfo = notification.userInfo!
+            
+            let keyboardFrame = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+            
+            let changeInHeight = (keyboardFrame.height + 40)
+            
+            self.scrollView.contentInset.bottom += changeInHeight
+            
+            self.scrollView.scrollIndicatorInsets.bottom += changeInHeight; self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    func resetContentInset() {
+        UIView.animate(withDuration: 0.2) {
+            self.scrollView.contentInset = .zero
+            self.scrollView.scrollIndicatorInsets = .zero
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.activeTextField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.activeTextField = textField
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
 }
-
